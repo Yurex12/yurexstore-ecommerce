b21eff73a3176ec075495333e7dac5d4345c9217
Yurex <yusufekungomi@gmail.com>
2026-01-23 00:39:54 +0100

    Configure Redis connection with host and port for production and development environments

--- backend/src/config/redisConnection.ts
+++ backend/src/config/redisConnection.ts
@@ -2,4 +2,6 @@ import IOredis from 'ioredis';
 
 export const connection = new IOredis({
   maxRetriesPerRequest: null,
+  host: process.env.REDIS_HOST!,
+  port: Number(process.env.REDIS_PORT!),
 });
--- backend/src/queue/paymentQueue.ts
+++ backend/src/queue/paymentQueue.ts
@@ -2,4 +2,9 @@ import { Queue } from 'bullmq';
 
 import { connection } from '../config/redisConnection';
 
-export const paymentQueue = new Queue('payments', { connection });
+export const paymentQueue = new Queue('payments', {
+  connection: {
+    host: process.env.REDIS_HOST!,
+    port: Number(process.env.REDIS_PORT!),
+  },
+});
